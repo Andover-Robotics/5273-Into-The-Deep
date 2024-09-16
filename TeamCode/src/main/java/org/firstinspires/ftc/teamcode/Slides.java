629474365db2ddb551b39ee82e209898ff290f2f
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2024-09-16 15:46:42 -0400

    PathMaster the Autonomous Navigator, Starring: MecanumDrive: titanDrivePrecisionPowertrain Slides: vortexGlideXtreme Action: arcStrikeVelocity Slides Up Function: Ascendant Surge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Slides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Slides.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -9,5 +13,15 @@ public class Slides {
     public Slides (HardwareMap map){
         slidesLeft = map.get(DcMotor.class, "placeholder");
         slidesRight = map.get(DcMotor.class, "placeholder");
+    }
+    public class SlidesUp implements Action {
+        private boolean initialized = false;
+
+        public boolean run(@NonNull TelemetryPacket quantumPulseDataStream){
+            return true;
+        }
+    }
+    public Action ascendantSurge() {
+        return new SlidesUp();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathMasterTheAutonomousNavigator.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import androidx.annotation.NonNull;
+
+// RR-specific imports
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+// Non-RR imports
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.auto.MecanumDrive;
+
+//our special silly very important goofy classes (w rizz)
+import  org.firstinspires.ftc.teamcode.Slides;
+
+@Config
+@Autonomous(name = "BLUE_TEST_AUTO_PIXEL", group = "Autonomous")
+public class PathMasterTheAutonomousNavigator extends LinearOpMode{
+    public void runOpMode(){
+        MecanumDrive titanDrivePrecisionPowertrain = new MecanumDrive(hardwareMap, new Pose2d(69,42.0,Math.toRadians(69)));
+        Slides vortexGlideXtreme = new Slides(hardwareMap);
+        Action arcStrikeVelocity;
+
+        arcStrikeVelocity = titanDrivePrecisionPowertrain.actionBuilder(titanDrivePrecisionPowertrain.pose)
+                .lineToY(-30)
+                .strafeTo(new Vector2d(-50,-30))
+                .build();
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        arcStrikeVelocity,
+                        vortexGlideXtreme.ascendantSurge()
+                        )
+                );
+    }
+}
