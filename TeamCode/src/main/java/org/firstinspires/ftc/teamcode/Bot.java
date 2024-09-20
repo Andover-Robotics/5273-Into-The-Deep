2670d302c778c354922da3dec845c0b78b4c7912
ethan <ethanlazar42@gmail.com>
2024-09-20 17:22:16 -0400

    added intake servo code stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
@@ -2,8 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import androidx.annotation.NonNull;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 /**
@@ -12,12 +14,15 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class Bot {
     private final Slides ascendingStorm;
     private final Movement godlikeManeuver;
+    private final CRServo intake;
 
     public Bot(@NonNull HardwareMap hardwareMap) {
         ascendingStorm = new Slides(hardwareMap);
         godlikeManeuver = new Movement(hardwareMap);
+        intake = hardwareMap.get(CRServo.class, "intake");
     }
     public void teleopTick(Gamepad gamepad1, Gamepad gamepad2, Telemetry telemetry){
         godlikeManeuver.teleopTick(gamepad1, telemetry);
+        intake.setPower(gamepad2.a ? 1 : 0);        // TODO: figure out if intake direction needs to be reversed
     }
 }
