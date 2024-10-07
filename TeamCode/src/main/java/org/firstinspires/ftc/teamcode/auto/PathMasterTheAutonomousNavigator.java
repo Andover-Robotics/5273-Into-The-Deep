23965dfb70dbc902330e33e62b46bda2143e8be8
andrewcodes28 <180860358+andrewcodes28@users.noreply.github.com>
2024-10-07 16:31:05 -0400

    test auto path

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathMasterTheAutonomousNavigator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathMasterTheAutonomousNavigator.java
@@ -33,16 +33,25 @@ public class PathMasterTheAutonomousNavigator extends LinearOpMode {
      * Runs the OpMode
     */
     public void runOpMode(){
-        MecanumDrive titanDrivePrecisionPowertrain = new MecanumDrive(hardwareMap, new Pose2d(0,0,Math.toRadians(90)));
+        MecanumDrive titanDrivePrecisionPowertrain = new MecanumDrive(hardwareMap, new Pose2d(-10, -60, Math.toRadians(90)));
         Slides ethan = new Slides(hardwareMap);
         Action arcStrikeVelocity;
 
         arcStrikeVelocity = titanDrivePrecisionPowertrain.actionBuilder(titanDrivePrecisionPowertrain.pose)
-                .lineToY(10)
-                .afterTime(1, () -> {
-                    telemetry.addData("moving", "yes");
-                    telemetry.update();
-                })
+                .lineToY(-35)
+                .waitSeconds(3)
+                .strafeTo(new Vector2d(-48,-38))
+                .waitSeconds(2)
+                .strafeToSplineHeading(new Vector2d(-55,-55),Math.toRadians(225))
+                .waitSeconds(2)
+                .strafeToSplineHeading(new Vector2d(-57.5,-38),Math.toRadians(90))
+                .waitSeconds(2)
+                .strafeToSplineHeading(new Vector2d(-55,-55),Math.toRadians(225))
+                .waitSeconds(2)
+                .strafeToSplineHeading(new Vector2d(-55,-38),Math.toRadians(180))
+                .strafeTo(new Vector2d(-55,-25))
+                .waitSeconds(2)
+                .strafeToSplineHeading(new Vector2d(-55,-55),Math.toRadians(225))
                 .build();
 
         waitForStart();
