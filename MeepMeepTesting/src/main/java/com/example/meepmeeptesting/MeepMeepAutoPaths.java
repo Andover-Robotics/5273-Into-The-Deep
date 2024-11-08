bb853ec09069a3242d3c8572cd0b5b339093d2c6
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2024-11-08 16:22:20 -0500

    Making beautiful methods for auto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepAutoPaths.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepAutoPaths.java
@@ -1,9 +1,11 @@
 package com.example.meepmeeptesting;
 
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -11,46 +13,12 @@ public class MeepMeepAutoPaths {
     public static void main(String[] args) {
         System.setProperty("sun.java2d.opengl", "true");
         MeepMeep meepMeep = new MeepMeep(600);
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-10, -60, Math.toRadians(90)))
-                .lineToY(-35)
-                .waitSeconds(3)
-                .strafeTo(new Vector2d(-48,-38))
-                .waitSeconds(2)
-                .strafeToSplineHeading(new Vector2d(-55,-55),Math.toRadians(225))
-                .waitSeconds(2)
-                .strafeToSplineHeading(new Vector2d(-57.5,-38),Math.toRadians(90))
-                .waitSeconds(2)
-                .strafeToSplineHeading(new Vector2d(-55,-55),Math.toRadians(225))
-                .waitSeconds(2)
-                .strafeToSplineHeading(new Vector2d(-55,-38),Math.toRadians(180))
-                .strafeTo(new Vector2d(-55,-25))
-                .waitSeconds(2)
-                .strafeToSplineHeading(new Vector2d(-55,-55),Math.toRadians(225))
-                .build());
 
-        RoadRunnerBotEntity myBot2 = new DefaultBotBuilder(meepMeep)
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .setColorScheme(new ColorSchemeBlueDark())
-                .build();
-        myBot2.runAction(myBot2.getDrive().actionBuilder(new Pose2d(10, 60, Math.toRadians(270)))
-                .lineToY(35)
-                .waitSeconds(3)
-                .strafeTo(new Vector2d(48,38))
-                .waitSeconds(2)
-                .strafeToSplineHeading(new Vector2d(55,55),Math.toRadians(45))
-                .waitSeconds(2)
-                .strafeToSplineHeading(new Vector2d(57.5,38),Math.toRadians(270))
-                .waitSeconds(2)
-                .strafeToSplineHeading(new Vector2d(55,55),Math.toRadians(45))
-                .waitSeconds(2)
-                .strafeToSplineHeading(new Vector2d(55,38),Math.toRadians(0))
-                .strafeTo(new Vector2d(55,25))
-                .waitSeconds(2)
-                .strafeToSplineHeading(new Vector2d(55,55),Math.toRadians(45))
-                .build());
+        RoadRunnerBotEntity myBot = quickBot(meepMeep,0);
+        myBot.runAction(getPathA(myBot,0));
+
+        RoadRunnerBotEntity myBot2 = quickBot(meepMeep, 2);
+        myBot2.runAction(getPathA(myBot2,2));
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
@@ -58,5 +26,42 @@ public class MeepMeepAutoPaths {
                 .addEntity(myBot2)
                 .addEntity(myBot)
                 .start();
+    }
+
+    private static RoadRunnerBotEntity quickBot(MeepMeep meepMeep,int quadrant){
+        return new DefaultBotBuilder(meepMeep)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setColorScheme(quadrant>= 2?new ColorSchemeRedDark() : new ColorSchemeBlueDark())
+                .build();
+    }
+
+    private static int getAngle(int angle, int quadrant) {
+        switch(quadrant) {
+            case 0: return angle;
+            case 1: return 180-angle;
+            case 2: return 180+angle;
+            case 3: return 360-angle;
+            default: return 0;
+        }
+    }
+    private static Action getPathA(RoadRunnerBotEntity myBot, int quadrant){
+        int xFactor = quadrant%3==0?1:-1;
+        int yFactor = quadrant>= 2?-1:1;
+        return myBot.getDrive().actionBuilder(new Pose2d(10*xFactor, 60*yFactor, Math.toRadians(getAngle(270,quadrant))))
+                .lineToY(35*yFactor)
+                .waitSeconds(3)
+                .strafeTo(new Vector2d(48*xFactor,38*yFactor))
+                .waitSeconds(2)
+                .strafeToSplineHeading(new Vector2d(55*xFactor,55*yFactor),Math.toRadians(getAngle(45,quadrant)))
+                .waitSeconds(2)
+                .strafeToSplineHeading(new Vector2d(57.5*xFactor,38*yFactor),Math.toRadians(getAngle(270, quadrant)))
+                .waitSeconds(2)
+                .strafeToSplineHeading(new Vector2d(55*xFactor,55*yFactor),Math.toRadians(getAngle(45,quadrant)))
+                .waitSeconds(2)
+                .strafeToSplineHeading(new Vector2d(55*xFactor,38*yFactor),Math.toRadians(getAngle(0,quadrant)))
+                .strafeTo(new Vector2d(55*xFactor,25*yFactor))
+                .waitSeconds(2)
+                .strafeToSplineHeading(new Vector2d(55*xFactor,55*yFactor),Math.toRadians(getAngle(45,quadrant)))
+                .build();
     }
 }
