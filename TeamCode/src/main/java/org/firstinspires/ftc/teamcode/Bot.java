b4c832f839bb6269bce86df7026c522b5dcbbcac
andrewcodes28 <180860358+andrewcodes28@users.noreply.github.com>
2024-12-13 16:37:29 -0500

    add basic opencv (part 1)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
@@ -2,12 +2,25 @@ package org.firstinspires.ftc.teamcode;
 
 import androidx.annotation.NonNull;
 
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.RotatedRect;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Represents the Bot.
  */
@@ -18,6 +31,49 @@ public class Bot {
     private final Slides slides;
     private final Pivot pivot;
     private final DiffyRotator diffyRotator;
+    private final OpenCvPipeline pipeline = new EmptyPipeline();
+    private final double APPROX_FACTOR = 0.04;
+    private final double MIN_RECT = 1000;
+
+    // OpenCV image processing
+    class EmptyPipeline extends OpenCvPipeline {
+        @Override
+        public Mat processFrame(Mat input) {
+            Mat gray = new Mat();
+            Imgproc.cvtColor(input, gray, Imgproc.COLOR_RGBA2GRAY);
+
+            // Blur
+            //Mat blur = new Mat();
+            //Img
+
+            Mat edges = new Mat();
+            Imgproc.Canny(gray, edges, 50, 150);
+
+            List<MatOfPoint> contours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+            Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            for (MatOfPoint contour: contours) {
+                MatOfPoint2f approx = new MatOfPoint2f();
+                MatOfPoint2f contour2f = new MatOfPoint2f(contour.toArray());
+                double epsilon = APPROX_FACTOR * Imgproc.arcLength(contour2f, true);
+                Imgproc.approxPolyDP(contour2f, approx, epsilon, true);
+
+                if (approx.rows() == 4) {
+                    Moments moments = Imgproc.moments(contour);
+                    double area = moments.get_m00();
+
+                    if (area > MIN_RECT) {
+                        RotatedRect rotated = Imgproc.minAreaRect(new MatOfPoint2f(contour.toArray()));
+                        // Do something with rotated.angle for orientation
+
+                    }
+                }
+            }
+
+            return input;
+        }
+    }
 
     public enum FSM{
         OUTTAKE, //outtake
@@ -37,6 +93,20 @@ public class Bot {
         slides = new Slides(hardwareMap);
         pivot = new Pivot(hardwareMap);
         diffyRotator = new DiffyRotator(hardwareMap);
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "NAME_OF_CAMERA_IN_CONFIG_FILE");
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                // Usually this is where you'll want to start streaming from the camera (see section 4)
+                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+            @Override
+            public void onError(int errorCode) {
+                // send help if this occurs
+            }
+        });
+        camera.setPipeline(pipeline);
     }
 
     /**
