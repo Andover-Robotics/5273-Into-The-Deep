6c8e4aea19dcfedd6ee24b9d38048a864e643ff7
andrewcodes28 <180860358+andrewcodes28@users.noreply.github.com>
2024-12-27 15:24:34 -0500

    add more constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
@@ -32,45 +32,60 @@ public class Bot {
     private final Slides slides;
     private final Pivot pivot;
     private final DiffyRotator diffyRotator;
-    private final OpenCvPipeline pipeline = new EmptyPipeline();
+    private final OpenCvPipeline pipeline = new RectPipeline();
+
+    // Factor to estimate a polygon with - shouldn't be too low because noise will impact
+    // the image more, and shouldn't be too high, or otherwise detail is lost
     private final double APPROX_FACTOR = 0.04;
+    // Makes sure that the rectangle is large enough and that it isn't noise
     private final double MIN_RECT = 1000;
+    // How many pixels should be blended together
+    private final int GAUSSIAN_BLUR = 5;
+    // Lower gradient bound for Canny edge detector
+    // https://en.wikipedia.org/wiki/Canny_edge_detector#Double_threshold
+    private final int CANNY_THRESHOLD_LOWER = 50;
+    // Higher bound for Canny edge detector
+    private final int CANNY_THRESHOLD_HIGHER = 150;
+    // Used for camera streaming - make sure this matches the camera's resolution
+    private final int CAMERA_WIDTH = 320;
+    private final int CAMERA_HEIGHT = 240;
 
     // OpenCV image processing
-    class EmptyPipeline extends OpenCvPipeline {
+    class RectPipeline extends OpenCvPipeline {
         @Override
         public Mat processFrame(Mat input) {
             Mat gray = new Mat();
             Imgproc.cvtColor(input, gray, Imgproc.COLOR_RGBA2GRAY);
 
-            // Blur
-            //Mat blur = new Mat();
-            //Img
+            // Reduce noise in outcome
             Mat blurredImage = new Mat();
-            Imgproc.GaussianBlur(gray, blurredImage, new Size(5, 5), 0);
-            Mat edges = new Mat();
-            Imgproc.Canny(blurredImage, edges, 50, 150);
+            Imgproc.GaussianBlur(gray, blurredImage, new Size(GAUSSIAN_BLUR, GAUSSIAN_BLUR), 0);
 
+            // Finds edge LINES
+            Mat edges = new Mat();
+            Imgproc.Canny(blurredImage, edges, CANNY_THRESHOLD_LOWER, CANNY_THRESHOLD_HIGHER);
+
+            // Finds edge POINTS
             List<MatOfPoint> contours = new ArrayList<>();
             Mat hierarchy = new Mat();
             Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
-
-
             for (MatOfPoint contour: contours) {
+                // Approximates a polygon from edge POINTS
                 MatOfPoint2f approx = new MatOfPoint2f();
                 MatOfPoint2f contour2f = new MatOfPoint2f(contour.toArray());
                 double epsilon = APPROX_FACTOR * Imgproc.arcLength(contour2f, true);
                 Imgproc.approxPolyDP(contour2f, approx, epsilon, true);
 
                 if (approx.rows() == 4) {
+                    // Zeroth moment means area (mass but height is 0)
                     Moments moments = Imgproc.moments(contour);
                     double area = moments.get_m00();
 
-                    if (area > MIN_RECT) {
+                    if (area >= MIN_RECT) {
+                        // Gets a rotated rectangle, because standard Rects are parallel to coordinate axes
                         RotatedRect rotated = Imgproc.minAreaRect(new MatOfPoint2f(contour.toArray()));
                         // Do something with rotated.angle for orientation
-
                     }
                 }
             }
@@ -102,8 +117,7 @@ public class Bot {
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
-                // Usually this is where you'll want to start streaming from the camera (see section 4)
-                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+                camera.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT);
             }
             @Override
             public void onError(int errorCode) {
