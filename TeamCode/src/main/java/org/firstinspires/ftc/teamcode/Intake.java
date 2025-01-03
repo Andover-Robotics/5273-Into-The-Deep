5a896a60f89339ae850537946d6b99d7ce08d8a2
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2025-01-03 16:25:38 -0500

    Replaced stop, changed names, added more four bar stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -5,14 +5,13 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Intake {
-    private final CRServo intake;
-    private final Arm arm;
-    private static final double RUN_POWER = 1;
     //private final CRServo intake;
     private final Servo fourL, fourR;
     private final Claw claw;
     private static final double CLAW_OPEN = 0, CLAW_CLOSED = 0;
     private static final double FOUR_INTAKE = 0, FOUR_OUTTAKE = 0;
+    private static final double ROLL_HORIZONTAL = 0, ROLL_VERTICAL = 0;
+    private static final double PITCH_INTAKE = 0, PITCH_TRANSFER= 0;
 
     public Intake (HardwareMap map) {
         //intake = map.get(CRServo.class, "iServo");
@@ -44,16 +43,11 @@ public class Intake {
     }
 
     public void posIntake(){
-        stop();
-        if (!(arm.fsm == Arm.ArmState.INTAKE)) arm.intakePos();
-        fsm = IntakeState.INTAKE_STOP;
+        fourTo(FOUR_INTAKE);
+        claw.clawRollPitch(ROLL_HORIZONTAL,PITCH_INTAKE);
     }
 
-    public void stop(){
-        intake.setPower(0);
-    }
-
-    public void runTransfer(){
+    public void openTransfer(){
         posTransfer();
         claw.openClaw();
         fsm = IntakeState.TRANSFER_OPEN;
@@ -66,9 +60,17 @@ public class Intake {
     }
 
     public void posTransfer(){
-        stop();
-        if (!(arm.fsm == Arm.ArmState.TRANSFER)) arm.transferPos();
-        fsm = IntakeState.TRANSFER_STOP;
+        fourTo(FOUR_OUTTAKE);
+        claw.clawRollPitch(ROLL_VERTICAL, PITCH_TRANSFER);
+    }
+
+    public void fourTo(double position){
+        fourL.setPosition(position);
+        fourR.setPosition(position);
+    }
+
+    public void moveRoll(boolean l, boolean r){
+        claw.rollActiveClaw(l,r);
     }
 
 }
