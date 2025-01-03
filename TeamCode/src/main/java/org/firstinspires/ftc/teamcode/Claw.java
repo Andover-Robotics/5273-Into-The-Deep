a8e67a963b46f7ffca52228c837ad231bc6aba7f
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2025-01-03 16:44:47 -0500

    redid claw with diffy implementation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import androidx.annotation.NonNull;
 
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -15,48 +14,42 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class Claw {
     private final Servo rotobot;
     private final DiffyRotator andrewLu;
-    public static final double OPEN_POS = 0.1861, CLOSED_POS = 0.0628;
-    public static final double UP_POS = 0.1, DOWN_POS = 0.2;
+    public static double openPos, closedPos;
     /**
      * Initializes a Claw instance.
      * @param hardwareMap {@link com.qualcomm.robotcore.hardware.HardwareMap}
      */
-    public Claw(@NonNull HardwareMap hardwareMap){
-        rotobot = hardwareMap.get(Servo.class, "clawServo");
-        andrewLu = new DiffyRotator(hardwareMap);
+    public Claw(@NonNull HardwareMap hardwareMap, double open, double closed, String name, String nameL, String nameR){
+        rotobot = hardwareMap.get(Servo.class, name);
+        andrewLu = new DiffyRotator(hardwareMap, nameL, nameR);
+        openPos = open;
+        closedPos = closed;
     }
 
-    public void clawUp() {andrewLu.roll(UP_POS);}
+    public void clawRollPitch(double rollDeg, double pitchDeg) {andrewLu.rollPitch(rollDeg,pitchDeg);}
 
-    public void clawDown() {andrewLu.roll(DOWN_POS);}
+    public void clawRoll(double rollDeg){andrewLu.rollToDegrees(rollDeg);}
 
-
+    public void clawPitch(double pitchDeg){andrewLu.pitchToDegrees(pitchDeg);}
     /**
      * Opens the claw.
      */
     public void openClaw(){
-        rotobot.setPosition(OPEN_POS);
+        rotobot.setPosition(openPos);
     }
 
     /**
      * Closes the claw.
      */
     public void closeClaw(){
-        rotobot.setPosition(CLOSED_POS);
+        rotobot.setPosition(closedPos);
     }
 
+    public void setClaw(double pos) {rotobot.setPosition(pos);}
 
-
-    /**
-     * Runs one tick of the Teleop Op Mode.
-     *  {@link com.qualcomm.robotcore.hardware.Gamepad} 2
-     * @param telemetry {@link org.firstinspires.ftc.robotcore.external.Telemetry}
-     */
-    public void teleopTick(float trigger, Telemetry telemetry){
-        if (trigger<=0.15 && !(rotobot.getPosition()==OPEN_POS)){
-            openClaw();
-        } else if (trigger > 0.15 && !(rotobot.getPosition()==CLOSED_POS)) {
-            closeClaw();
-        }
+    public void rollActiveClaw(boolean left, boolean right){
+        if (left && !right) clawRoll(andrewLu.roll+15);
+        else if (right && !left) clawRoll(andrewLu.roll+15);
     }
+
 }
