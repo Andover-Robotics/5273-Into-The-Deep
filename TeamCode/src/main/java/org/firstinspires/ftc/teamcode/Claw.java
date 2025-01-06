47a251fd5d3b7b5a00fb328e69db1b5a65a7f9f4
naushadwaqar <nwaqar970209@gmail.com>
2025-01-06 15:49:49 -0500

    added a function to check if the claw has a sample returns bool

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import androidx.annotation.NonNull;
@@ -15,15 +16,18 @@ public class Claw {
     private final Servo rotobot;
     private final DiffyRotator andrewLu;
     public static double openPos, closedPos;
+    private ColorSensor colorSensor;
     /**
      * Initializes a Claw instance.
      * @param hardwareMap {@link com.qualcomm.robotcore.hardware.HardwareMap}
      */
-    public Claw(@NonNull HardwareMap hardwareMap, double open, double closed, String name, String nameL, String nameR){
+    public Claw(@NonNull HardwareMap hardwareMap, double open, double closed, String name, String nameL, String nameR)
+    {
         rotobot = hardwareMap.get(Servo.class, name);
         andrewLu = new DiffyRotator(hardwareMap, nameL, nameR);
         openPos = open;
         closedPos = closed;
+        colorSensor = hardwareMap.get(ColorSensor.class, "colorSensor");
     }
 
     public void clawRollPitch(double rollDeg, double pitchDeg) {andrewLu.rollPitch(rollDeg,pitchDeg);}
@@ -51,5 +55,18 @@ public class Claw {
         if (left && !right) clawRoll(andrewLu.roll+15);
         else if (right && !left) clawRoll(andrewLu.roll+15);
     }
-
+        // sample colors red, blue and yellow yellow = #FFFF00
+     //This means that the range for each color value is from 0 to 4095 (2^12 = 4096).
+    //TODO: fine tune values
+    public boolean hasSample()
+    {
+        int red,blue,green;
+        red = colorSensor.red();
+        blue = colorSensor.blue();
+        green = colorSensor.green();
+        return(
+                (red >= 2500 && blue <= 500 && green <= 500) ||
+                (red <= 500 && blue >= 2500 && green <= 500) ||
+                 (red >= 2500 && blue <= 500 && green >= 2500));
+    }
 }
