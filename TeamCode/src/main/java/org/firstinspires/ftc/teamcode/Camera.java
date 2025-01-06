aa471488504d4084d2db908e33b6823041fe45c1
andrewcodes28 <180860358+andrewcodes28@users.noreply.github.com>
2025-01-06 16:40:40 -0500

    did some stuff?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camera.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
@@ -26,25 +27,32 @@ public class Camera {
     private volatile RotatedRect result = null;
     // Factor to estimate a polygon with - shouldn't be too low because noise will impact
     // the image more, and shouldn't be too high, or otherwise detail is lost
-    private final double APPROX_FACTOR = 0.04;
+    private final double APPROX_FACTOR = 0.06;
     // Makes sure that the rectangle is large enough and that it isn't noise
     private final double MIN_RECT = 1000;
     // How many pixels should be blended together
     private final int GAUSSIAN_BLUR = 5;
     // Lower gradient bound for Canny edge detector
     // https://en.wikipedia.org/wiki/Canny_edge_detector#Double_threshold
-    private final int CANNY_THRESHOLD_LOWER = 50;
+    private final int CANNY_THRESHOLD_LOWER = 40;
     // Higher bound for Canny edge detector
-    private final int CANNY_THRESHOLD_HIGHER = 150;
+    private final int CANNY_THRESHOLD_HIGHER = 130;
     // Used for camera streaming - make sure this matches the camera's resolution
     private final int CAMERA_WIDTH = 320;
     private final int CAMERA_HEIGHT = 240;
+    private final int COLOR_BOUND = 170;
+    private final Scalar bound1 = new Scalar(0, COLOR_BOUND, 0);
+    private final Scalar bound2 = new Scalar(0, 0, COLOR_BOUND);
+    private final Scalar highBound = new Scalar(255, 255, 255);
 
     // OpenCV image processing
     class RectPipeline extends OpenCvPipeline {
         private Mat gray = new Mat();
         private Mat blurredImage = new Mat();
-        private Mat edges = new Mat();
+        private Mat mask1 = new Mat();
+        private Mat mask2 = new Mat();
+        private Mat interMask = new Mat();
+        private Mat colorResult = new Mat();
         private Mat hierarchy = new Mat();
         private MatOfPoint2f approx = new MatOfPoint2f();
         private MatOfPoint2f contour2f = new MatOfPoint2f();
@@ -53,17 +61,25 @@ public class Camera {
         public Mat processFrame(Mat input) {
             result = null;
 
-            Imgproc.cvtColor(input, gray, Imgproc.COLOR_RGBA2GRAY);
+            Imgproc.cvtColor(input, gray, Imgproc.COLOR_RGB2YCrCb);
 
             // Reduce noise in outcome
             Imgproc.GaussianBlur(gray, blurredImage, new Size(GAUSSIAN_BLUR, GAUSSIAN_BLUR), 0);
 
-            // Finds edge LINES
-            Imgproc.Canny(blurredImage, edges, CANNY_THRESHOLD_LOWER, CANNY_THRESHOLD_HIGHER);
+            // Finds red
+            Core.inRange(blurredImage, bound1, highBound, colorResult);
+
+            // TODO Why does this crash?
+            /*
+            Core.inRange(blurredImage, bound1, highBound, mask1);
+            Core.inRange(blurredImage, bound2, highBound, mask2);
+            Core.bitwise_or(mask1, mask2, interMask);
+            Core.bitwise_and(blurredImage, blurredImage, colorResult, interMask);
+             */
 
             // Finds edge POINTS
             List<MatOfPoint> contours = new ArrayList<>();
-            Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+            Imgproc.findContours(colorResult, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
             for (MatOfPoint contour: contours) {
                 // Approximates a polygon from edge POINTS
@@ -71,7 +87,7 @@ public class Camera {
                 double epsilon = APPROX_FACTOR * Imgproc.arcLength(contour2f, true);
                 Imgproc.approxPolyDP(contour2f, approx, epsilon, true);
 
-                if (approx.rows() == 4) {
+                /*if (approx.rows() == 4)*/ {
                     // Zeroth moment means area (mass but height is 0)
                     Moments moments = Imgproc.moments(contour);
                     double area = moments.get_m00();
