cc254c5e2d05e97e63ccf6c825a4d089eac47267
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2025-01-21 23:40:37 -0500

    Outtake arm testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/IntakeValueTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/IntakeValueTester.java
@@ -14,9 +14,20 @@ public class IntakeValueTester extends LinearOpMode {
         intake = new Intake(hardwareMap,new Camera(hardwareMap, telemetry));
         waitForStart();
         while(opModeIsActive()){
-            if (gamepad1.left_stick_y!=0){
-                intake.fourLTo(Math.signum(gamepad1.left_stick_y)*0.05);
-                intake.fourRTo(Math.signum(gamepad1.left_stick_y)*0.05);
+            vSlides.slidesMove(gp2.getLeftY(), gp2.getButton(GamepadKeys.Button.B), telemetry);
+            telemetry.addData("vSlides position",vSlides.getEncoders());
+            if (gp1.getButton(GamepadKeys.Button.A)){
+                /*intake.fourLTo(0.106);
+                intake.fourRTo(0.2);*/
+                outtake.fourLTo(0.1);
+                outtake.fourRTo(0.9);
+
+            }
+            if (gp1.getButton(GamepadKeys.Button.B)){
+                /*intake.fourLTo(0.503); //top position : 0.503
+                intake.fourRTo(1); // top position : 1*/
+                outtake.fourLTo(1);
+                outtake.fourRTo(0);
             }
             intake.moveRoll(gamepad1.dpad_left,gamepad1.dpad_right);
             intake.movePitch(gamepad1.dpad_up,gamepad1.dpad_down);
