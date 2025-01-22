630fd6c603539af5600328a904cd1389d5f402c8
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2025-01-21 23:41:57 -0500

    intake tester imports + gamepad ex

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/IntakeValueTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/IntakeValueTester.java
@@ -5,12 +5,26 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Camera;
 import org.firstinspires.ftc.teamcode.Intake;
+import org.firstinspires.ftc.teamcode.Outtake;
+import org.firstinspires.ftc.teamcode.SlidesHorizontal;
+import org.firstinspires.ftc.teamcode.SlidesVertical;
+import com.arcrobotics.ftclib.command.button.GamepadButton;
+import com.arcrobotics.ftclib.command.button.Trigger;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.TriggerReader;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 @TeleOp(name = "Intake Value Tester", group = "Teleop")
 public class IntakeValueTester extends LinearOpMode {
     Intake intake;
+    GamepadEx gp1;
+    GamepadEx gp2;
     @Override
     public void runOpMode(){
+        gp1 = new GamepadEx(gamepad1);
+        gp2 = new GamepadEx(gamepad2);
         intake = new Intake(hardwareMap,new Camera(hardwareMap, telemetry));
         waitForStart();
         while(opModeIsActive()){
@@ -31,6 +45,9 @@ public class IntakeValueTester extends LinearOpMode {
             }
             intake.moveRoll(gamepad1.dpad_left,gamepad1.dpad_right);
             intake.movePitch(gamepad1.dpad_up,gamepad1.dpad_down);
+            if (gp1.getButton(GamepadKeys.Button.Y)){
+            if (gp1.isDown(GamepadKeys.Button.LEFT_BUMPER)){
+            if (gp1.isDown(GamepadKeys.Button.RIGHT_BUMPER)){
             intake.clawActive(gamepad1.right_stick_y);
             telemetry.addData("Intake Arm Position Left: ",intake.fourLPos());
             telemetry.addData("Intake Arm Position Right: ",intake.fourRPos());
