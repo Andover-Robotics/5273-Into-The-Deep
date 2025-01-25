2da07689a852bf8e5614a7f7b83df979fed2a52b
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2025-01-25 00:42:05 -0500

    Making sure that it doesnt drop a pixel while holding one in survey position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
@@ -96,7 +96,7 @@ public class Bot {
                     intake.closeIntake();
                     Thread.sleep(300);
                 }
-                else if (!(intake.fsm == Intake.IntakeState.INTAKE_CLOSED)&& !(gamepad2.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER)>0.1)){
+                else if (!(intake.fsm == Intake.IntakeState.INTAKE_CLOSED || intake.fsm == Intake.IntakeState.SURVEY_CLOSED)&& !(gamepad2.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER)>0.1)){
                     intake.openSurvey();
                 }
                 if(intake.fsm == Intake.IntakeState.INTAKE_OPEN && (gamepad2.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER)>0.1)){
