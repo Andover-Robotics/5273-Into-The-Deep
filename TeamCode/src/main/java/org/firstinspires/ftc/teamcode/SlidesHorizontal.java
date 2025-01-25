3c065a9df867b772d5a4de6a7ceea54b66c06dae
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2025-01-24 23:59:05 -0500

    hSlides limits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlidesHorizontal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlidesHorizontal.java
@@ -20,7 +20,8 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class SlidesHorizontal {
     private final Servo slidesLeft, slidesRight;
 
-    private static final double EXPANDED = 1.0, CONTRACTED = 0.0;
+    private static final double EXPANDEDR = 0.5383, CONTRACTEDR = 1.0;
+    private static final double EXPANDEDL = 0.4567, CONTRACTEDL = 0.0;
 
     public SlidesHorizontal(HardwareMap map) {
         slidesLeft = map.get(Servo.class, "slidesHL");
@@ -40,37 +41,41 @@ public class SlidesHorizontal {
         slidesRight.setPosition(position);
     }
 
-    public double getPositions(){
-        return(slidesLeft.getPosition()+ slidesRight.getPosition())/2.0;
+    public void setRight(double pos){
+        slidesRight.setPosition(pos);
+    }
+
+    public void setLeft(double pos){
+        slidesLeft.setPosition(pos);
+    }
+
+    public double getRight(){
+        return(slidesRight.getPosition());
+    }
+
+    public double getLeft(){
+        return(slidesLeft.getPosition());
     }
 
     public void setPower(double power){
-        //if (getPositions()+(Math.signum(power)*0.01) >= EXPANDED) open();
-        //else if (getPositions()+(Math.signum(power)*0.01) <= CONTRACTED) close();
-        //else {
-            slidesRight.setPosition(slidesRight.getPosition() + (Math.signum(power)*0.01));
-            slidesLeft.setPosition(slidesLeft.getPosition() + (Math.signum(power)*(-0.01)));
+        if (((getLeft()+Math.signum(power)*-0.005)>=EXPANDEDL) || ((getRight()+Math.signum(power)*0.005)<=EXPANDEDR)) open();
+        else if (((getLeft()+Math.signum(power)*-0.005)<=CONTRACTEDL) || ((getRight()+Math.signum(power)*0.005)>=CONTRACTEDR)) close();
+        else {
+            setRight(getRight()+Math.signum(power)*0.005);
+            setLeft(getLeft()+Math.signum(power)*-0.005);
             fsm = HSlides.MIDDLE;
-        //}
-    }
-
-    public void slidesMove(double input, boolean overrideButton, Telemetry telemetry){
-        double pos = (getPositions());
-        telemetry.addData("Horizontal Slides position: ",pos);
-        if(!overrideButton && ((pos > CONTRACTED && input > 0) || (pos < EXPANDED && input < 0) )) {
-            setPower(0);
-        }else{
-            setPower(input);
         }
     }
 
     public void close() {
-        setBothPosition(CONTRACTED);
+        setRight(CONTRACTEDR);
+        setLeft(CONTRACTEDL);
         fsm = HSlides.IN;
     }
 
     public void open() {
-        setBothPosition(EXPANDED);
+        setRight(EXPANDEDR);
+        setLeft(EXPANDEDL);
         fsm = HSlides.OUT;
     }
 }
