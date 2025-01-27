1f3189a9b45773aade31f45b3858a5e2ad4fa741
andrewcodes28 <180860358+andrewcodes28@users.noreply.github.com>
2025-01-27 16:59:37 -0500

    new auto paths v3

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepAutoPaths.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepAutoPaths.java
@@ -14,30 +14,42 @@ public class MeepMeepAutoPaths {
         System.setProperty("sun.java2d.opengl", "true");
         MeepMeep meepMeep = new MeepMeep(600);
 
-        RoadRunnerBotEntity myBot = quickBot(meepMeep,0);
+        RoadRunnerBotEntity myBot = quickBot(meepMeep,0, false);
 
-        RoadRunnerBotEntity myBot1 = quickBot(meepMeep,1);
+        RoadRunnerBotEntity myBot1 = quickBot(meepMeep,1, true);
+        RoadRunnerBotEntity myBot1a = quickBot(meepMeep,1, false);
 
-        RoadRunnerBotEntity myBot2 = quickBot(meepMeep, 2);
+        RoadRunnerBotEntity myBot2 = quickBot(meepMeep, 2, false);
 
-        RoadRunnerBotEntity myBot3 = quickBot(meepMeep,3);
+        RoadRunnerBotEntity myBot3 = quickBot(meepMeep,3, true);
+        RoadRunnerBotEntity myBot3a = quickBot(meepMeep,3, false);
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
                 .addEntity(myBot1)
+                .addEntity(myBot1a)
                 .addEntity(myBot2)
                 .addEntity(myBot3)
+                .addEntity(myBot3a)
                 .start();
     }
 
-    private static RoadRunnerBotEntity quickBot(MeepMeep meepMeep,int quadrant){
+    private static RoadRunnerBotEntity quickBot(MeepMeep meepMeep,int quadrant, boolean optimize){
         RoadRunnerBotEntity bot = new DefaultBotBuilder(meepMeep)
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .setColorScheme(quadrant>= 2?new ColorSchemeRedDark() : new ColorSchemeBlueDark())
                 .build();
-        bot.runAction(quadrant % 2 == 0 ? getPathBucket(bot, quadrant) : getPathSpecimen(bot, quadrant));
+        Action action = null;
+        if (optimize) {
+            action = getPathSpecimenOptimized(bot, quadrant);
+        } else if (quadrant % 2 == 0) {
+            action = getPathBucket(bot, quadrant);
+        } else {
+            action = getPathSpecimen(bot, quadrant);
+        }
+        bot.runAction(action);
         return bot;
     }
 
@@ -49,6 +61,33 @@ public class MeepMeepAutoPaths {
             case 3: return 360-angle;
             default: return 0;
         }
+    }
+
+    private static Action getPathSpecimenOptimized(RoadRunnerBotEntity myBot, int quadrant) {
+        int xFactor = quadrant%3==0?1:-1;
+        int yFactor = quadrant>=2?-1:1;
+        return myBot.getDrive().actionBuilder(new Pose2d(10*xFactor, 60*yFactor, Math.toRadians(getAngle(270,quadrant))))
+                .strafeTo(new Vector2d(40 * xFactor, 45 * yFactor))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(50 * xFactor, 45 * yFactor))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(60 * xFactor, 45 * yFactor))
+                .waitSeconds(1)
+                .strafeToSplineHeading(new Vector2d(60 * xFactor, 60 * yFactor), Math.toRadians(getAngle(90, quadrant)))
+                .waitSeconds(0.5)
+                .strafeToSplineHeading(new Vector2d(8 * xFactor, 33 * yFactor), Math.toRadians(getAngle(270, quadrant)))
+                .waitSeconds(1)
+                .strafeToSplineHeading(new Vector2d(60 * xFactor, 60 * yFactor), Math.toRadians(getAngle(90, quadrant)))
+                .waitSeconds(0.5)
+                .strafeToSplineHeading(new Vector2d(8 * xFactor, 33 * yFactor), Math.toRadians(getAngle(270, quadrant)))
+                .waitSeconds(1)
+                .strafeToSplineHeading(new Vector2d(60 * xFactor, 60 * yFactor), Math.toRadians(getAngle(90, quadrant)))
+                .waitSeconds(0.5)
+                .strafeToSplineHeading(new Vector2d(8 * xFactor, 33 * yFactor), Math.toRadians(getAngle(270, quadrant)))
+                .waitSeconds(1)
+                .strafeToSplineHeading(new Vector2d(60 * xFactor, 60 * yFactor), Math.toRadians(getAngle(90, quadrant)))
+                .waitSeconds(0.5)
+                .build();
     }
 
     private static Action getPathSpecimen(RoadRunnerBotEntity myBot, int quadrant) {
