8677a20d5c3e8c5106ff477ae91407c1eae0cbea
andrewcodes28 <180860358+andrewcodes28@users.noreply.github.com>
2025-01-28 11:45:34 -0500

    refactor auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathMasterTheTestingNavigator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathMasterTheTestingNavigator.java
@@ -3,8 +3,10 @@ package org.firstinspires.ftc.teamcode.auto;
 // RR-specific imports
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.acmerobotics.roadrunner.Vector2d;
 
@@ -12,8 +14,10 @@ import com.acmerobotics.roadrunner.Vector2d;
 // Non-RR imports
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 //our special silly very important goofy classes (w rizz)
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Camera;
 import org.firstinspires.ftc.teamcode.Outtake;
 import org.firstinspires.ftc.teamcode.Intake;
@@ -22,15 +26,7 @@ import org.firstinspires.ftc.teamcode.SlidesVertical;
 /**
  * Yet another OpMode, this time for Autonomous - the names are intentional (and great), don't mess with them
  */
-@Config
-@Autonomous(name = "Autonomous", group = "Autonomous")
-public class PathMasterTheTestingNavigator extends LinearOpMode {
-    /**
-     * Runs the OpMode
-    */
-
-
-
+public class PathMasterTheTestingNavigator {
     private static int getAngle(int angle, int quadrant) {
         switch (quadrant) {
             case 0:
@@ -46,93 +42,81 @@ public class PathMasterTheTestingNavigator extends LinearOpMode {
         }
     }
 
-    public void runOpMode(){
+    private static Intake intake;
+    private static Outtake outtake;
+    private static SlidesVertical verticalSlides;
+
+    public static void runOpMode(LinearOpMode opMode, int quadrant){
+        HardwareMap hardwareMap = opMode.hardwareMap;
+        Telemetry telemetry = opMode.telemetry;
+
         MecanumDrive mecanumDrive = new MecanumDrive(hardwareMap, new Pose2d(-10, -60, Math.toRadians(90)));
-        SlidesVertical slidesVertical = new SlidesVertical(hardwareMap);
-        Action arcStrikeVelocity;
 
-        Intake intake = new Intake(hardwareMap, new Camera(hardwareMap, telemetry));
-        Outtake outtake = new Outtake(hardwareMap);
-        SlidesVertical verticalSlides = new SlidesVertical(hardwareMap);
-
-
-        int quadrant = 1;
+        intake = new Intake(hardwareMap, new Camera(hardwareMap, telemetry));
+        outtake = new Outtake(hardwareMap);
+        verticalSlides = new SlidesVertical(hardwareMap);
 
         int xFactor = quadrant%3==0?1:-1;
         int yFactor = quadrant>=2?-1:1;
 
-        arcStrikeVelocity =mecanumDrive.actionBuilder(new Pose2d(10*xFactor, 60*yFactor, Math.toRadians(getAngle(270,quadrant))))
+        Action arcStrikeVelocity = mecanumDrive.actionBuilder(new Pose2d(10*xFactor, 60*yFactor, Math.toRadians(getAngle(270,quadrant))))
                 .strafeTo(new Vector2d(48 * xFactor, 38 * yFactor))
                 // input sample 1
-                .stopAndAdd(intake::openIntake)
-                .waitSeconds(0.5)
-                .stopAndAdd(intake::posIntake)
-                .stopAndAdd(intake::closeIntake)
-                .waitSeconds(0.5)
-                .stopAndAdd(intake::looseClaw)
-                .stopAndAdd(intake::posTransfer)
+                .stopAndAdd(doIntake())
                 .strafeToSplineHeading(new Vector2d(60 * xFactor, 60 * yFactor), Math.toRadians(getAngle(45, quadrant)))
                 // output sample 1
-                .waitSeconds(0.5)
-                .stopAndAdd(outtake::openTransfer)
-                .waitSeconds(0.5)
-                .stopAndAdd(outtake::close)
-                .waitSeconds(0.5)
-                .stopAndAdd(verticalSlides::moveToUpperBound)
-                .stopAndAdd(outtake::closeBucket)
-                .waitSeconds(0.5)
-                .stopAndAdd(outtake::open)
-                .stopAndAdd(verticalSlides::moveToLowerBound)
+                .stopAndAdd(doOuttake())
                 .strafeToSplineHeading(new Vector2d(58 * xFactor, 38 * yFactor), Math.toRadians(getAngle(270, quadrant)))
                 //input sample 2
-                .stopAndAdd(intake::openIntake)
-                .waitSeconds(0.5)
-                .stopAndAdd(intake::posIntake)
-                .stopAndAdd(intake::closeIntake)
-                .waitSeconds(0.5)
-                .stopAndAdd(intake::looseClaw)
-                .stopAndAdd(intake::posTransfer)
+                .stopAndAdd(doIntake())
                 .strafeToSplineHeading(new Vector2d(60 * xFactor, 60 * yFactor), Math.toRadians(getAngle(45, quadrant)))
                 // output sample 2
-                .waitSeconds(0.5)
-                .stopAndAdd(outtake::openTransfer)
-                .waitSeconds(0.5)
-                .stopAndAdd(outtake::close)
-                .waitSeconds(0.5)
-                .stopAndAdd(verticalSlides::moveToUpperBound)
-                .stopAndAdd(outtake::closeBucket)
-                .waitSeconds(0.5)
-                .stopAndAdd(outtake::open)
-                .stopAndAdd(verticalSlides::moveToLowerBound)
+                .stopAndAdd(doOuttake())
                 .strafeToSplineHeading(new Vector2d(69 * xFactor, 38 * yFactor), Math.toRadians(getAngle(270, quadrant)))
                 // input sample 3
-                .stopAndAdd(intake::openIntake)
-                .waitSeconds(0.5)
-                .stopAndAdd(intake::posIntake)
-                .stopAndAdd(intake::closeIntake)
-                .waitSeconds(0.5)
-                .stopAndAdd(intake::looseClaw)
-                .stopAndAdd(intake::posTransfer)
+                .stopAndAdd(doIntake())
                 .strafeToSplineHeading(new Vector2d(60 * xFactor, 60 * yFactor), Math.toRadians(getAngle(45, quadrant)))
                 // output sample 3
-                .waitSeconds(0.5)
-                .stopAndAdd(outtake::openTransfer)
-                .waitSeconds(0.5)
-                .stopAndAdd(outtake::close)
-                .waitSeconds(0.5)
-                .stopAndAdd(verticalSlides::moveToUpperBound)
-                .stopAndAdd(outtake::closeBucket)
-                .waitSeconds(0.5)
-                .stopAndAdd(outtake::open)
-                .stopAndAdd(verticalSlides::moveToLowerBound)
+                .stopAndAdd(doOuttake())
                 .build();
 
-        waitForStart();
+        opMode.waitForStart();
 
-        Actions.runBlocking(
-                new SequentialAction(
-                        arcStrikeVelocity
-                )
+        Actions.runBlocking(arcStrikeVelocity);
+    }
+
+    private static Action doIntake() {
+        return new SequentialAction(
+                new InstantAction(intake::openIntake),
+                new SleepAction(0.5),
+                new InstantAction(() -> {
+                    intake.posIntake();
+                    intake.closeIntake();
+                }),
+                new SleepAction(0.5),
+                new InstantAction(() -> {
+                    intake.looseClaw();
+                    intake.posTransfer();
+                })
+        );
+    }
+
+    private static Action doOuttake() {
+        return new SequentialAction(
+                new SleepAction(0.5),
+                new InstantAction(outtake::openTransfer),
+                new SleepAction(0.5),
+                new InstantAction(outtake::close),
+                new SleepAction(0.5),
+                new InstantAction(() -> {
+                    verticalSlides.moveToUpperBound();
+                    outtake.closeBucket();
+                }),
+                new SleepAction(0.5),
+                new InstantAction(() -> {
+                    outtake.open();
+                    verticalSlides.moveToLowerBound();
+                })
         );
     }
 
