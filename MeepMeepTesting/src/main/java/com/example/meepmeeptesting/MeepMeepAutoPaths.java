be9fea4f9047288a3bd0f438bc71915f3305ad3b
andrewcodes28 <180860358+andrewcodes28@users.noreply.github.com>
2025-01-29 14:34:50 -0500

    add park auto as an option

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepAutoPaths.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepAutoPaths.java
@@ -14,38 +14,40 @@ public class MeepMeepAutoPaths {
         System.setProperty("sun.java2d.opengl", "true");
         MeepMeep meepMeep = new MeepMeep(600);
 
-        RoadRunnerBotEntity myBot = quickBot(meepMeep,0, false);
+        RoadRunnerBotEntity myBot = quickBot(meepMeep,0, 2);
 
-        RoadRunnerBotEntity myBot1 = quickBot(meepMeep,1, true);
-        RoadRunnerBotEntity myBot1a = quickBot(meepMeep,1, false);
+        RoadRunnerBotEntity myBot1 = quickBot(meepMeep,1, 2);
+        //RoadRunnerBotEntity myBot1a = quickBot(meepMeep,1, 1);
 
-        RoadRunnerBotEntity myBot2 = quickBot(meepMeep, 2, false);
+        RoadRunnerBotEntity myBot2 = quickBot(meepMeep, 2, 2);
 
-        RoadRunnerBotEntity myBot3 = quickBot(meepMeep,3, true);
-        RoadRunnerBotEntity myBot3a = quickBot(meepMeep,3, false);
+        RoadRunnerBotEntity myBot3 = quickBot(meepMeep,3, 2);
+        //RoadRunnerBotEntity myBot3a = quickBot(meepMeep,3, 1);
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
                 .addEntity(myBot1)
-                .addEntity(myBot1a)
+                //.addEntity(myBot1a)
                 .addEntity(myBot2)
                 .addEntity(myBot3)
-                .addEntity(myBot3a)
+                //.addEntity(myBot3a)
                 .start();
     }
 
-    private static RoadRunnerBotEntity quickBot(MeepMeep meepMeep,int quadrant, boolean optimize){
+    private static RoadRunnerBotEntity quickBot(MeepMeep meepMeep,int quadrant, int type){
         RoadRunnerBotEntity bot = new DefaultBotBuilder(meepMeep)
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .setColorScheme(quadrant>= 2?new ColorSchemeRedDark() : new ColorSchemeBlueDark())
                 .build();
         Action action = null;
-        if (optimize) {
-            action = getPathSpecimenOptimized(bot, quadrant);
+        if (type == 2) {
+            action = getPark(bot, quadrant);
         } else if (quadrant % 2 == 0) {
             action = getPathBucket(bot, quadrant);
+        } else if (type == 1) {
+            action = getPathSpecimenOptimized(bot, quadrant);
         } else {
             action = getPathSpecimen(bot, quadrant);
         }
@@ -61,6 +63,14 @@ public class MeepMeepAutoPaths {
             case 3: return 360-angle;
             default: return 0;
         }
+    }
+
+    private static Action getPark(RoadRunnerBotEntity myBot, int quadrant) {
+        int xFactor = quadrant%3==0?1:-1;
+        int yFactor = quadrant>=2?-1:1;
+        return myBot.getDrive().actionBuilder(new Pose2d(10*xFactor, 60*yFactor, Math.toRadians(getAngle(270,quadrant))))
+                .strafeTo(new Vector2d(60 * xFactor, 60 * yFactor))
+                .build();
     }
 
     private static Action getPathSpecimenOptimized(RoadRunnerBotEntity myBot, int quadrant) {
@@ -88,7 +98,7 @@ public class MeepMeepAutoPaths {
                 .strafeToSplineHeading(new Vector2d(8 * xFactor, 33 * yFactor), Math.toRadians(getAngle(270, quadrant)))
                 .waitSeconds(1)
                 .strafeToSplineHeading(new Vector2d(60 * xFactor, 60 * yFactor), Math.toRadians(getAngle(90, quadrant)))
-                .waitSeconds(0.5)
+                //.waitSeconds(0.5)
                 .build();
     }
 
@@ -121,7 +131,7 @@ public class MeepMeepAutoPaths {
                 .strafeToSplineHeading(new Vector2d(8 * xFactor, 33 * yFactor), Math.toRadians(getAngle(270, quadrant)))
                 .waitSeconds(0.5)
                 .strafeToSplineHeading(new Vector2d(60 * xFactor, 60 * yFactor), Math.toRadians(getAngle(90, quadrant)))
-                .waitSeconds(0.5)
+                //.waitSeconds(0.5)
                 .build();
     }
 
@@ -142,7 +152,7 @@ public class MeepMeepAutoPaths {
                 .strafeToSplineHeading(new Vector2d(69 * xFactor, 38 * yFactor), Math.toRadians(getAngle(270, quadrant)))
                 .waitSeconds(0.5)
                 .strafeToSplineHeading(new Vector2d(60 * xFactor, 60 * yFactor), Math.toRadians(getAngle(45, quadrant)))
-                .waitSeconds(0.5)
+                //.waitSeconds(0.5)
                 .build();
     }
 }
