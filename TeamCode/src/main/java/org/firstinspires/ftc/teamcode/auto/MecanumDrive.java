a4d9d6bca6f2ccee1ce5f30f1f7d48d16be3243b
tango8 <ethan.b.tang@gmail.com>
2025-02-10 17:38:47 -0500

    increase speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MecanumDrive.java
@@ -73,13 +73,13 @@ public final class MecanumDrive {
         public double kA = 0.00008;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 20;
-        public double minProfileAccel = -20;
-        public double maxProfileAccel = 20;
+        public double maxWheelVel = 60;
+        public double minProfileAccel = -60;
+        public double maxProfileAccel = 60;
 
         // turn profile parameters (in radians)
-        public double maxAngVel = Math.PI/2; // shared with path
-        public double maxAngAccel = Math.PI/2;
+        public double maxAngVel = Math.PI; // shared with path
+        public double maxAngAccel = Math.PI;
 
         // path controller gains
         public double axialGain = 5.0;
