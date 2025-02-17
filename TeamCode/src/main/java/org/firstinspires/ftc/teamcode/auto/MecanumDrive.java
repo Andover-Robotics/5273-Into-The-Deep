12914483615e09db8c1d86a8fdd0fb137c8fd513
tango8 <ethan.b.tang@gmail.com>
2025-02-17 16:25:31 -0500

    comented out slides stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MecanumDrive.java
@@ -73,9 +73,9 @@ public final class MecanumDrive {
         public double kA = 0.00008;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 60;
-        public double minProfileAccel = -60;
-        public double maxProfileAccel = 60;
+        public double maxWheelVel = 40;
+        public double minProfileAccel = -40;
+        public double maxProfileAccel = 40;
 
         // turn profile parameters (in radians)
         public double maxAngVel = Math.PI; // shared with path
@@ -86,9 +86,9 @@ public final class MecanumDrive {
         public double lateralGain = 1.0;
         public double headingGain = 5.0; // shared with turn
 
-        public double axialVelGain = 1.0;
-        public double lateralVelGain = 2.0;
-        public double headingVelGain = 0.5; // shared with turn
+        public double axialVelGain = 0;
+        public double lateralVelGain = 0;
+        public double headingVelGain = 0; // shared with turn
     }
 
     public static Params PARAMS = new Params();
