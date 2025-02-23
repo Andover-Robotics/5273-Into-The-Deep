cb2d6e9661f280ba82d865913dfaacd139e08dce
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2025-02-23 16:24:04 -0500

    redid all outtake arm positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outtake.java
@@ -6,8 +6,8 @@ public class Outtake {
     private final Servo armL, armR;
     private final Servo claw;
     private static final double CLAW_OPEN = 0.1472, CLAW_CLOSED = 0.00;
-    private static final double ARML_BUCKET = 0.81611, ARML_TRANSFER = 0.295, ARML_CLIP = 0.90667;
-    private static final double ARMR_BUCKET = 0.18055, ARMR_TRANSFER = 0.698889, ARMR_CLIP = 0.08944;
+    private static final double ARML_BUCKET = 0.81611, ARML_TRANSFER = 0.185, ARML_CLIP = 8755555;
+    private static final double ARMR_BUCKET = 0.18055, ARMR_TRANSFER = 0.81167, ARMR_CLIP = 0.122277;
 
     public Outtake (HardwareMap map) {
         //intake = map.get(CRServo.class, "iServo");
