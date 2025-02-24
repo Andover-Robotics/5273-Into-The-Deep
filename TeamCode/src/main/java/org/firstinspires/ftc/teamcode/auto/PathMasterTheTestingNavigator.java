93722ac221a0948bd73d3defc7c7742ace8ff5da
tango8 <ethan.b.tang@gmail.com>
2025-02-24 16:14:55 -0500

    spec park is back to backwards again just like spec auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathMasterTheTestingNavigator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathMasterTheTestingNavigator.java
@@ -78,9 +78,9 @@ public class PathMasterTheTestingNavigator {
         verticalSlides = new SlidesVertical(opMode);
         bot = new Bot(opMode,hardwareMap, telemetry);
 
-        Vector2d intakeSample1 = new Vector2d(-38, 24);
-        Vector2d intakeSample2 = new Vector2d(-50, 24);
-        Vector2d intakeSample3 = new Vector2d(-51, 26);
+        Vector2d intakeSample1 = new Vector2d(-39.5, 26);
+        Vector2d intakeSample2 = new Vector2d(-51.5, 26);
+        Vector2d intakeSample3 = new Vector2d(-52.5, 28);
 
         Vector2d outtakeBucket = new Vector2d(-44.5, 10 );
 
