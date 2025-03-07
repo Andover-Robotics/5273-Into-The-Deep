1559351ca488f1a8e236b87bf2eda9f7e5806fe1
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2025-03-06 20:55:30 -0500

    this might explain why outtake arm is slightly tweaking during in clip pos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outtake.java
@@ -6,7 +6,7 @@ public class Outtake {
     private final Servo armL, armR;
     private final Servo claw;
     private static final double CLAW_OPEN = 0.1472, CLAW_CLOSED = 0.00;
-    private static final double ARML_BUCKET = 0.81611, ARML_TRANSFER = 0.185, ARML_CLIP = 8755555;
+    private static final double ARML_BUCKET = 0.81611, ARML_TRANSFER = 0.185, ARML_CLIP = .8755555;
     private static final double ARMR_BUCKET = 0.18055, ARMR_TRANSFER = 0.81167, ARMR_CLIP = 0.122277;
 
     public Outtake (HardwareMap map) {
