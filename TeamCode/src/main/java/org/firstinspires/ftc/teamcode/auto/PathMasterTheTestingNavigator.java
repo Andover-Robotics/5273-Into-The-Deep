e65ed7ae2c5e1ac1ea40c647ca3169d8a39d72d4
tango8 <ethan.b.tang@gmail.com>
2025-03-09 15:22:39 -0400

    bucket auto stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathMasterTheTestingNavigator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathMasterTheTestingNavigator.java
@@ -65,9 +65,9 @@ public class PathMasterTheTestingNavigator {
 
         bot = new Bot(opMode,hardwareMap, telemetry);
 
-        Vector2d intakeSample1 = new Vector2d(-20, 26.5);
+        Vector2d intakeSample1 = new Vector2d(-19.5, 26.5);
         Vector2d intakeSample2 = new Vector2d(-29, 24.75);
-        Vector2d intakeSample3 = new Vector2d(-27.5, 35.75);
+        Vector2d intakeSample3 = new Vector2d(-27.75, 35.75);
 
         Vector2d outtakeBucket = new Vector2d(-23.5, 6.5);
 
@@ -113,7 +113,7 @@ public class PathMasterTheTestingNavigator {
                 .stopAndAdd(bot.looseHori())
                 .stopAndAdd(bot.clawRoll90())
                 // output sample 2
-                .strafeToSplineHeading(new Vector2d(-25, 35.75), Math.toRadians(180))
+                .strafeToSplineHeading(new Vector2d(-25.5, 35.75), Math.toRadians(180))
                 .strafeToSplineHeading(intakeSample3, Math.toRadians(180))
                 .waitSeconds(.5)
                 //input sample 3
