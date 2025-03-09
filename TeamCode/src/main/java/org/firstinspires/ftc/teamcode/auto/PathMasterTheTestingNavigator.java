fb4307f9b19ba224059cb5f4e031bf5494d0fa3f
tango8 <ethan.b.tang@gmail.com>
2025-03-09 15:24:06 -0400

    bucket auto stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathMasterTheTestingNavigator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathMasterTheTestingNavigator.java
@@ -67,10 +67,9 @@ public class PathMasterTheTestingNavigator {
 
         bot = new Bot(opMode,hardwareMap, telemetry);
 
-        Vector2d intakeSample1 = new Vector2d(-20, 26.5);
+        Vector2d intakeSample1 = new Vector2d(-19.5, 26.5);
         Vector2d intakeSample2 = new Vector2d(-29, 24.75);
-        Vector2d intakeSample3 = new Vector2d(-27.5, 35.75);
-
+        Vector2d intakeSample3 = new Vector2d(-27.75, 35.75);
         //Vector2d outtakeBucket = new Vector2d(-25, 5);
 
 
@@ -105,7 +104,7 @@ public class PathMasterTheTestingNavigator {
                                 new SleepAction(.5),
                                 doSlidesUpBucket()
                         ),
-                        mecanumDrive.actionBuilder(new Pose2d(-20, 26.5, Math.toRadians(90)))
+                        mecanumDrive.actionBuilder(new Pose2d(-19.5, 26.5, Math.toRadians(90)))
                                 .strafeToSplineHeading(new Vector2d(-25.5, 4.5), Math.toRadians(45))
                                 .build()
                 ))
@@ -159,7 +158,7 @@ public class PathMasterTheTestingNavigator {
                                 new SleepAction(.5),
                                 doSlidesUpBucket()
                         ),
-                        mecanumDrive.actionBuilder(new Pose2d(-27.5, 35.75, Math.toRadians(90)))
+                        mecanumDrive.actionBuilder(new Pose2d(-27.75, 35.75, Math.toRadians(90)))
                                 .strafeToSplineHeading(new Vector2d(-25.5, 4.5), Math.toRadians(45))
                                 .build()
                 ))
