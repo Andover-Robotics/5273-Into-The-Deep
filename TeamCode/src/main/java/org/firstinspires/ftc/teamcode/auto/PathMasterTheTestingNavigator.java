34c2a85df2028b77ebcfcc3b8af391d418e0979c
tango8 <ethan.b.tang@gmail.com>
2025-03-09 23:37:05 -0400

    parks should be good,starrting orientation is the same as its respective actual auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathMasterTheTestingNavigator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathMasterTheTestingNavigator.java
@@ -36,7 +36,7 @@ public class PathMasterTheTestingNavigator {
 
 
         Action arcStrikeVelocity = mecanumDrive.actionBuilder(new Pose2d(0 , 0 , Math.toRadians(90)))
-                .strafeTo(new Vector2d(-55, 0))
+                .strafeTo(new Vector2d(-28, 0))
                 .build();
         opMode.waitForStart();
         Actions.runBlocking(arcStrikeVelocity);
@@ -51,7 +51,7 @@ public class PathMasterTheTestingNavigator {
         bot = new Bot(opMode,hardwareMap, telemetry);
 
         Action arcStrikeVelocity = mecanumDrive.actionBuilder(new Pose2d(0 , 0 , Math.toRadians(270)))
-                .strafeTo(new Vector2d(55, 0))
+                .strafeTo(new Vector2d(36, 0))
                 .build();
         opMode.waitForStart();
         Actions.runBlocking(arcStrikeVelocity);
