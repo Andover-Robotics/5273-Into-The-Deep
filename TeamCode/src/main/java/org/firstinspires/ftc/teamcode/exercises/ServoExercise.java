46d1d96ff0eb8a4045213faf817111915143184b
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2025-05-05 15:04:20 -0400

    exercise for newbies

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exercises/ServoExercise.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.exercises;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class ServoExercise
+{
+}
